f35ec190c3c15a9d6144eaeec04d288e1b8a8663
DungDev1204 <tiendung.dev12@gmail.com>
2025-05-20 00:19:36 +0700

    makefile

---
+++ build/CMakeFiles/wallet_system.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wallet_system.dir/main.cpp.obj"
+  "CMakeFiles/wallet_system.dir/main.cpp.obj.d"
+  "libwallet_system.dll.a"
+  "wallet_system.exe"
+  "wallet_system.exe.manifest"
+  "wallet_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wallet_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
